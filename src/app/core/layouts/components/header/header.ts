27756cc1b149860fedc0aee0d277e26eb981ba7e
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-06-04 18:13:06 -0500

    feat: implement upgrade header2

--- src/app/core/layouts/components/header/header.ts
+++ src/app/core/layouts/components/header/header.ts
@@ -1,34 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
 })
-export class Header implements OnInit {
-  currentTitle = 'Dashboard';
-
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
-
-  ngOnInit() {
-    this.updateTitle();
-  
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      this.updateTitle();
-    });
-  }
-
-  private updateTitle() {
-    let route = this.activatedRoute.root;
-    while (route.firstChild) {
-      route = route.firstChild;
-    }
-    
-    if (route.snapshot.data && route.snapshot.data['title']) {
-      this.currentTitle = route.snapshot.data['title'];
-    }
-  }
-}
+export class Header {}
