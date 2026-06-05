191b9169f3f797f072c426d364608a36138bcf42
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-06-04 20:42:37 -0500

    feat: implement contact page

--- src/app/features/dashboard/pages/contact/contact.ts
+++ src/app/features/dashboard/pages/contact/contact.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './contact.html',
 })
-export class Contact {}
+export class Contact {
+  developerName = 'Luis Fernando Chumbes Ramos';
+  developerRole = 'Ingeniero de Sistemas / Desarrollador Full-Stack';
+  developerEmail = 'luisfernando3chr@gmail.com';
+  githubUrl = 'https://github.com/Luis3Fernando';
+  linkedinUrl = 'https://www.linkedin.com/in/luis-fernando3/';
+  photoPath = 'assets/images/developer.jpeg'; 
+}
